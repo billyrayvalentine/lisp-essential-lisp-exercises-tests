7a98087d5b984d5d350d6911bcb202216730f1e2
Billy Ray <billyrayvalentine@users.noreply.github.com>
2023-12-16 14:03:56 +0000

    chore: More tests (#2)

--- chapter3.lisp
+++ chapter3.lisp
@@ -17,3 +17,21 @@
 
 (test 3dot3
   (is (equal '(nil t) (ele:numline -5))))
+
+(test 3dot4
+  (is (equal 'cat (ele:carlis '(cat rabbit))))
+  (is (equal 'george (ele:carlis 'george)))
+  (is-false (ele:carlis nil)))
+
+(test 3dot5
+  (is (equal ele::'hot (ele:checktemp 100)))
+  (is (equal ele::'medium (ele:checktemp 60)))
+  (is (equal ele::'cold (ele:checktemp 20))))
+
+(test 3dot6
+  (is (equal '(x) (ele:my-make-list 'x)))
+  (is (equal '(a b) (ele:my-make-list '(a b))))
+  (is (equal nil (ele:my-make-list nil)))
+  (is (equal '() (ele:my-make-list '()))))
+
+(test 3dot7)
